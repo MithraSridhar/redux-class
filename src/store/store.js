54daab42409cb30212a75e094e227d753d24475f
mithrasridhar <mithugopal30@gmail.com>
2023-11-30 10:59:55 +0530

    changes

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import expenditureReducer from "../redux/expSlice"
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    //providing name for reducer as myExpenditure
+   myExpenditure: expenditureReducer
+  },
 })
